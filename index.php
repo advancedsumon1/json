f9acbe9f508fd412a027b1dc07b1f8c5a61c0b29
Advanced <advancedappit@gmail.com>
2018-06-04 02:40:53 +0600

    php2json

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php   
+           $connect = mysqli_connect("localhost", "root", "", "a");  
+           $sql = "SELECT * FROM sumon";  
+           $result = mysqli_query($connect, $sql);  
+           $json_array = array();  
+           while($row = mysqli_fetch_assoc($result))  
+           {  
+                $json_array[] = $row;  
+           }  
+           /*echo '<pre>';  
+           print_r(json_encode($json_array));  
+           echo '</pre>';*/  
+           echo json_encode($json_array);  
+           ?>  
